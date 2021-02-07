0bafc1dbedf9e152d626e14e6e1a56e3c9690c4d
Giuseppe Scrivano <gscrivan@redhat.com>
2021-02-07 20:13:44 +0100

    tests: do not create files under /tmp
    Signed-off-by: Giuseppe Scrivano <gscrivan@redhat.com>

--- tests/helpers.bash
+++ tests/helpers.bash
@@ -2,9 +2,9 @@
 
 function setup {
     ID=$(head -c 32 /dev/urandom | sha256sum | cut -f 1 -d ' ')
-    PROGRAM=$BATS_TMPDIR/program-$ID
-    BPF=$BATS_TMPDIR/bpf-$ID
-    RESULT=$BATS_TMPDIR/result-$ID
+    PROGRAM=$BATS_RUN_TMPDIR/program-$ID
+    BPF=$BATS_RUN_TMPDIR/bpf-$ID
+    RESULT=$BATS_RUN_TMPDIR/result-$ID
 
     if $(test $(uname -m) != x86_64); then
         skip "tests assume x86_64"
